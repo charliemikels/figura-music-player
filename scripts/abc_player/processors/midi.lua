ffd46a9a62571271a027ac92bdf8a1be3af4085d
Charlie Mikels <charliemikels@gmail.com>
2023-06-22 14:18:20 -0400

    Converted bitwise opperators to functions. Figura didn't recognize the `<<`, `|`, `&` syntax. Hopefully I got the order of opperations correct. That will be a pain to debug if it's wrong.

--- scripts/abc_player/processors/midi.lua
+++ scripts/abc_player/processors/midi.lua
@@ -1,3 +1,5 @@
+-- I stole this from https://github.com/Possseidon/lua-midi
+
 ---Reads exactly count bytes from the given stream, raising an error if it can't.
 ---@param stream file* The stream to read from.
 ---@param count integer The count of bytes to read.
@@ -19,8 +21,8 @@ local function readVLQ(stream)
   local length = 0
   repeat
     local byte = assert(stream:read(1), "incomplete or missing variable length quantity"):byte()
-    value = value << 7
-    value = value | byte & 0x7F
+    value = bit.lshift(value, 7)
+    value = bit.bor(value, bit.band(byte, 0x7F))
     length = length + 1
   until byte < 0x80
   return value, length
@@ -63,7 +65,7 @@ local midiEvent = {
   end,
   [0xE0] = function(stream, callback, channel, fb)
     local lsb, msb = ("I1I1"):unpack(fb .. stream:read(1))
-    callback("pitch", channel, (lsb | msb << 7) / 0x2000 - 1)
+    callback("pitch", channel, (bit.lshift(bit.bor(lsb, msb), 7)) / 0x2000 - 1)
     return 2
   end
 }
@@ -111,7 +113,7 @@ local metaEvents = {
   [0x54] = makeForwarder("smpteOffset"),
   [0x58] = function(data, callback)
     local numerator, denominator, metronome, dotted = (">I1I1I1I1"):unpack(data)
-    callback("timeSignature", numerator, 1 << denominator, metronome, dotted)
+    callback("timeSignature", numerator, bit.lshift(1, denominator), metronome, dotted)
   end,
   [0x59] = function(data, callback)
     local count, minor = (">I1I1"):unpack(data)
@@ -184,7 +186,7 @@ local function processEvent(stream, callback, runningStatus)
 
 
   if status >= 0x80 and status < 0xF0 then
-    length = length + midiEvent[status & 0xF0](stream, callback, (status & 0x0F) + 1, firstByte)
+    length = length + midiEvent[bit.band(status, 0xF0)](stream, callback, bit.band(status, 0x0F) + 1, firstByte)
   elseif status == 0xF0 then
     length = length + sysexEvent(stream, callback, firstByte)
   elseif status == 0xF2 then
