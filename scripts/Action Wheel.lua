52e9550fcae91c530afc5daf967542c53fcc2289
Charlie Mikels <charliemikels@gmail.com>
2025-01-28 12:38:17 -0500

    some basic library code, and api archetecture

--- scripts/Action Wheel.lua
+++ scripts/Action Wheel.lua
@@ -1,5 +1,8 @@
 local root_action_wheel_page = action_wheel:newPage()
 action_wheel:setPage(root_action_wheel_page)
-root_action_wheel_page:setAction(-1, require("scripts/abc_player/abc_player"))
+-- root_action_wheel_page:setAction(-1, require("scripts/abc_player/abc_player"))
+
+local midiPlayerScriptApi = require("scripts/abc_player/midi_player")
+midiPlayerScriptApi:build_MusicPlayer()
 
 return root_action_wheel_page
--- scripts/abc_player/midi_player.lua
+++ scripts/abc_player/midi_player.lua
@@ -2,28 +2,145 @@
 -- Music Player V5.0.0-beta.1 (Midi)
 
 events.ENTITY_INIT:register(function ()
-	print("=== Dev init: ".. client.getSystemTime() .." ===")
+	print("=== MIDI - Entity init: ".. client.getSystemTime() .." ===")
 end)
+
+-- Debug flags --
+local disable_abc = true
+
+
+
+-- Defaults --
+
+-- Default Library Path
+local default_songbook_path = "TL_Songbook"
+
+
+-- Defining a bunch of types -- -----------------------------------------------
+
+---Everything you need to control the music player
+---@class MusicPlayerAPI
+---@field add_library fun(path: string) Adds all song files found in `path` to the songbook.
+
 
 ---@class MusicPlayer
 ---@field library SongLibrary
+---@field api MusicPlayerAPI
 
 
----@class SongLibrary
 ---The Song library is the authoritative source of song data. Both the song list, paths, and raw data.
+---@class SongLibrary
 ---@field songs SongEntry[]
----
 
 ---@class SongEntry
 ---@field truepath string The authoritative path given to the files API
 ---@field name string The name used in the displayed song list
----@field name_decorators string[] Extra strings or icons to append to the name. These will be displayed in the song picker
 ---@field short_name string The name used when displayed to others
+---@field library string the path to the library where the song lives.
 ---@field data nil|table
+---@field file_type ( "" | "abc" | "midi" )
 
----comment
----@return SongLibrary
-local function build_song_library()
 
-    return {}
+
+---@class MusicPlayerBuilderOptions
+---@field libraryPaths string[]
+
+
+
+
+---A meta api to control the music player script.
+---This api is returned at the end of the script to wherever it was required.
+---Then it's the caller's job to create the MusicPlayer.
+---@class MusicPlayerScriptAPI
+---@field build_empty_MusicPlayer fun(self: MusicPlayerScriptAPI): MusicPlayer
+---@field build_MusicPlayer fun(self: MusicPlayerScriptAPI): MusicPlayer
+local script_api = {}
+
+---Creates a blank, bare-bones, music player.
+---Use script_api:build_MusicPlayer() to get a ready-to-go music player.
+---@return MusicPlayer
+function script_api:build_empty_MusicPlayer()
+    local music_player  -- pre-initilize music_player so that we can use in inside API calls
+
+    ---@type MusicPlayer
+    music_player = {
+        instruments = {},
+        library = {
+            songs = {},
+        },
+        api = {
+            add_library = function (library_path)
+                if not host:isHost() then
+                    error("Viewer script attempted to load a song library")
+                    return
+                end
+
+                if not file:isPathAllowed(library_path) then
+                    error("Attempted to load a path that is not allowed: " .. tostring(library_path))
+                    return
+                end
+
+                if not file:isDirectory(library_path) then
+                    error("Path is not a directory" .. tostring(library_path))
+                    return
+                end
+
+                local song_paths_to_test = file:list(library_path);
+
+                while #song_paths_to_test > 0 do
+                    local short_path = table.remove(song_paths_to_test)
+                    local full_path = library_path .. "/" .. short_path
+
+                    if file:isDirectory(full_path) then
+                    -- Path is a directory, put its contents into the test loop.
+                    for _, sub_path in ipairs(file:list(full_path)) do
+                        table.insert(song_paths_to_test, (short_path .. "/" .. sub_path))
+                    end
+                    elseif file:isFile(full_path) then
+                        -- TODO: Add file to library
+
+                        local file_ext = full_path:match("%.([^%.]+)$"):lower()
+
+                        local file_type = ""
+                        if file_ext == "abc" then
+                            file_type = "abc"
+                        elseif file_ext == "mid" or file_ext == "midi" then
+                            file_type = "midi"
+                        end
+
+                        if file_type == "midi" or (not disable_abc and file_type ~= "abc") then
+                            music_player.library.songs[full_path] = {
+                                data_source = "files",
+                                library = library_path,
+                                name = short_path,
+                                truepath = full_path,
+                                short_name = short_path:match("([^/]*)%."),
+                                    -- gets just the name of the file without dirs or extensions.
+                                file_type = file_type,
+                                -- data = nil
+                            }
+                        end
+                    end
+                end
+            end,
+        }
+    }
+
+    return music_player
 end
+
+---@param self MusicPlayerScriptAPI
+---@param options MusicPlayerBuilderOptions?
+---@return MusicPlayer
+function script_api:build_MusicPlayer(options)
+    local music_player = self:build_empty_MusicPlayer()
+    music_player.api.add_library("TL_Songbook")
+
+    printTable(music_player.library.songs["TL_Songbook/MM/games/Wii Sports - Theme.mid"])
+    return music_player
+end
+
+
+
+
+return script_api
