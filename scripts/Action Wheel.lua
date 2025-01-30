b78725060a6ed33100cccc1db85464183bd1ec35
Charlie Mikels <charliemikels@gmail.com>
2025-01-30 11:13:08 -0500

    renamed midi_player to music_player. Sepparating out midi-speciffic stuff into it's on require-able script.

--- scripts/Action Wheel.lua
+++ scripts/Action Wheel.lua
@@ -2,7 +2,7 @@ local root_action_wheel_page = action_wheel:newPage()
 action_wheel:setPage(root_action_wheel_page)
 -- root_action_wheel_page:setAction(-1, require("scripts/abc_player/abc_player"))
 
-local midiPlayerScriptApi = require("scripts/music_player/midi_player")
+local midiPlayerScriptApi = require("scripts/music_player/music_player")
 local music_player_api = midiPlayerScriptApi:build_default_MusicPlayer()
 
 -- printTable(music_player_api.get_song_by_sorted_index(1))
