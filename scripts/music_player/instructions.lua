7b569c68731797ae63d64f9bf570cac92af6a6d7
Charlie Mikels <charliemikels@gmail.com>
2025-05-29 17:33:25 -0400

    added a TODO warning

--- scripts/music_player/instructions.lua
+++ scripts/music_player/instructions.lua
@@ -1,6 +1,6 @@
 ---@class Instruction
 ---@field track_index integer       Used by instruments system. Track 0 is a "meta track" and applies song-level changes like tempo and time signature changes
----@field start_time number
+---@field start_time number         -- TODO: Start times at full midi resolution get large fast. we probably should store this as a delta, and let the network system find ways to include "landmark" times for each packet.
 ---@field duration number           May be 0
 ---@field note number               The note to play, or ID of meta event
 ---@field modifiers NoteModifier[]  TODO: Modify note during playback, or if end_time is 0 used for metadata
