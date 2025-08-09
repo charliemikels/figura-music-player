d6c52d6b98fe52da000c500771263b0402812f80
Charlie Mikels <charliemikels@gmail.com>
2025-08-09 16:59:09 -0400

    packet_type, durration, and track percussion info are now being serialized.

--- scripts/music_player/packages.lua
+++ scripts/music_player/packages.lua
@@ -63,17 +63,31 @@ end
 local function bytes_to_string_with_len_from_reader(reader)
     local len_string = vlq_to_int_from_packet(reader)
     local str = string.char(table.unpack( reader.bytes, reader.index, reader.index+len_string-1 ))
+    reader.index = reader.index + len_string
     return str
 end
 
+--- Effectively converts 5 → `101` → {1, 0, 1}.
+--- If expected_len > results_list, append false to left side (`101` → `00101`)
+--- Used to assign percussion tracks to incomming songs
+local function int_to_bit_list(int, length)
+    local bits = {}
+    for bit_index = length, 1, -1 do
+        local bit = math.fmod(int, 2)
+        bits[bit_index] = bit
+        int = math.floor((int - bit) / 2)
+    end
+    return bits
+end
 
+--- Effectively converts {true, false, true} → `101` → 5
+local function bit_list_to_number(bits)
+    return tonumber(table.concat(bits), 2)
+end
 
-
----@alias PacketStream {read_index: integer, bytes: integer[]}
 
 ---@alias SongDataPacket Byte[]
 ---@alias SongHeaderPacket Byte[]
-
 ---@alias SongPacket SongDataPacket|SongHeaderPacket
 
 ---A helper that wraps a list of bytes with an index,
@@ -82,13 +96,17 @@ end
 local function new_packet_reader(bytes)
     ---@class PacketReader
     local reader = {
-        ---@type SongDataPacket
-        bytes = bytes,
-        ---@type integer
-        index = 1,
+        bytes = bytes,  ---@type SongDataPacket
+        index = 1,      ---@type integer
     }
     return reader
 end
+
+local packet_ids = {
+    header = 1, -- Includeds initial like name, durration, track_types
+    data = 2,   -- Bulk of the packet stream
+    config = 3, -- A packet that might appear to update a song's configuration.
+}
 
 ---comment
 ---@param processed_song ProcessedSong
@@ -97,9 +115,6 @@ end
 local function song_to_packets(processed_song, player_config)
     printTable(processed_song)
 
-    local my_string = "Hi! 🎉"
-    printTable({string.byte(my_string, 1, -1 )})
-
     local header_packet = {}
 
     ---A unique ID for each song since the avatar loaded.
@@ -107,11 +122,23 @@ local function song_to_packets(processed_song, player_config)
     songs_turned_into_packets_so_far = songs_turned_into_packets_so_far +1
     union_tables(header_packet, transfered_song_id)
 
+    union_tables(header_packet, int_to_vlq(packet_ids.header))
+
     union_tables(header_packet, string_to_bytes_with_len(processed_song.name))
 
+    union_tables(header_packet, int_to_vlq(
+        math.ceil(processed_song.durration) -- At 144FPS, the player can only update every 5ms ceil to drop sub-milisecond precission.
+    ))
+
+    local track_type_bits = {}
+    for _, track in ipairs(processed_song.tracks) do
+        table.insert(track_type_bits, track.instrument_type_id)
+    end
+    union_tables(header_packet, int_to_vlq(#track_type_bits))
+    union_tables(header_packet, int_to_vlq(bit_list_to_number(track_type_bits)))
 
 
-
+    -- TODO: append a buffer time to the end of header_packet
     return { header_packet }
 end
 
@@ -122,17 +149,32 @@ local function add_packet_to_song(packet_data)
     local transfered_song_id = vlq_to_int_from_packet(reader)
     print("transfered_song_id", transfered_song_id)
 
-    -- todo: if transfered_song_id is known, read as data packet. else: header packet.
-    local is_header_packet = true
-    if is_header_packet then
-        local song_name = bytes_to_string_with_len_from_reader(reader)
-        print(song_name)
+    local packet_id = vlq_to_int_from_packet(reader)
+
+    if packet_id == packet_ids.header then
+        local name = bytes_to_string_with_len_from_reader(reader)
+        local durration = vlq_to_int_from_packet(reader)
+
+        local num_tracks = vlq_to_int_from_packet(reader)
+        local track_type_id_flags = int_to_bit_list(vlq_to_int_from_packet(reader), num_tracks)
+        ---@type Track[]
+        local tracks = {}
+        for i, type in ipairs(track_type_id_flags) do
+            tracks[i] = {instrument_type_id = type}
+        end
+
+        ---@type ProcessedSong
+        local incomming_processed_song = {
+            name = name,
+            durration = durration,
+            tracks = tracks,
+            instructions = {},
+            buffer_delay = 0,
+            buffer_start_time = client:getSystemTime()
+        }
+
+        printTable(incomming_processed_song)
     end
-
-
-
-    -- local song_title_length = vlq_to_int_from_packet(reader)
-    -- print("song_title_length", song_title_length)
 
 end
 
