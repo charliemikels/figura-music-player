2f33610ae05b3cd4172a045063a0d7e8b1bdec0b
Charlie Mikels <charliemikels@gmail.com>
2026-03-27 16:15:10 -0400

    Quick fix where build_default_library would be called on non-host clients.

--- scripts/Action Wheel.lua
+++ scripts/Action Wheel.lua
@@ -18,9 +18,10 @@ action_wheel:setPage(root_action_wheel_page)
 --       https://discord.com/channels/1129805506354085959/1340798228165300224/1340798228165300224
 --       /give @p minecraft:player_head[minecraft:profile={id:[I;1039887675,1961051688,-1756947787,-2031944347],name:"Drum"}]
 
-local ui_api = require("scripts/music_player/ui")
-local default_library = require("scripts/music_player/libraries"):build_default_library()
-local enter_music_player_action_wheel_ui = ui_api.new_action_wheel_ui(default_library)
-root_action_wheel_page:setAction(-1, enter_music_player_action_wheel_ui )
-
+if host:isHost() then   -- TODO: it's host only b/c build_default_library() calls file API. should we instead have build_default_library() skip filesAPI if non-host? (possibly allows for local songs)
+    local ui_api = require("scripts/music_player/ui")
+    local default_library = require("scripts/music_player/libraries"):build_default_library()
+    local enter_music_player_action_wheel_ui = ui_api.new_action_wheel_ui(default_library)
+    root_action_wheel_page:setAction(-1, enter_music_player_action_wheel_ui )
+end
 return root_action_wheel_page
