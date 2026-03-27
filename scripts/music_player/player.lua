e7f99590af394c2e74f17639a2f652be737fb40b
Charlie Mikels <charliemikels@gmail.com>
2026-03-27 13:01:20 -0400

    Removed a TODO. This TODO is important for the ping stuff, but when in memory, the size of start time doesn't _really_ matter too much.
    It might for like, saveing the file to disk localy, or for uploading
    with the avatar, but thats for later.

--- scripts/music_player/player.lua
+++ scripts/music_player/player.lua
@@ -29,7 +29,7 @@ local function print_host(...) if host:isHost() or do_debug_prints then print(..
 
 ---@class Instruction
 ---@field track_index integer       Used by instruments system. Track 0 is a "meta track" and applies song-level changes like tempo and time signature changes
----@field start_time number         -- TODO: Start times at full midi resolution get large fast. we probably should store this as a delta, and let the network system find ways to include "landmark" times for each packet.
+---@field start_time number
 ---@field start_velocity number     The initial velocity (volume) of the note.
 ---@field duration number           May be 0
 ---@field note number               The note to play, or ID of meta event
