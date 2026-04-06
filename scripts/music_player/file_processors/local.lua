713afd8c8f739da7f61249919381e80de3a53565
charlie <charlie@charliemikels.com>
2026-04-06 19:28:21 -0400

    local song discovery

--- scripts/music_player/file_processors/local.lua
+++ scripts/music_player/file_processors/local.lua
@@ -1,3 +1,36 @@
+local function escape_patern_matching_magic_characters(string_to_escape)
+    return string_to_escape:gsub("([^%w])", "%%%1")
+end
+
+---@class LocalSongScript
+---@field data PackedSongPacket[]
+---@field name string
+
+local song_script_returns = {}
+
+local local_songs_directory_path = "./local_songs"
+for _, script in pairs(listFiles(local_songs_directory_path, true)) do
+    local require_success, require_return = pcall(function() return require(script) end)
+    if not require_success then
+        print("Error: Failed to require local song script `"..script.."`. This script will be skipped. Full error below:\n\n"..tostring(require_return))
+    else
+        ---@cast require_return LocalSongScript
+        if require_return and type(require_return) == "table" and require_return.data and require_return.name then
+            print("Found local song:", require_return.name)
+            table.insert(song_script_returns, require_return)
+        else
+            print(tostring(script).." exists, but does not look like a local song")
+        end
+    end
+end
+
+local local_songs = {}      ---@type Song[]
+for _, script in pairs(song_script_returns) do
+    -- TODO: song_script_return.data to songs. Should we start the processor here, or on demand with pings? (On demand would be like a 3rd way to dispatch songs. (ping to process))
+end
+
+
+
 
 ---@type table<integer, boolean>
 local libraries_we_have_already_added_local_songs_to = {}
