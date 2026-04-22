7db615fcb5db5320451bdde05b5b83dc79ad1fba
charlie <charlie@charliemikels.com>
2026-04-22 01:13:49 -0400

    tried running the formatter to see what it would do.

--- scripts/Action Wheel.lua
+++ scripts/Action Wheel.lua
@@ -1,13 +1,12 @@
-
 local root_action_wheel_page = action_wheel:newPage()
 action_wheel:setPage(root_action_wheel_page)
 -- root_action_wheel_page:setAction(-1, require("scripts/abc_player/abc_player"))
 
 if host:isHost() then
     local default_library = require("scripts/music_player/libraries"):build_default_library()
-    local song = default_library:get_song_by_sorted_index(14)      -- 10: rush e full. 14: Starbound Atlas
+    local song = default_library:get_song_by_sorted_index(14) -- 10: rush e full. 14: Starbound Atlas
     local song_processor_future = song:start_data_processor()
-    song_processor_future:register_callback(function (_)
+    song_processor_future:register_callback(function(_)
         -- print("Instruction test")
         -- local starting_ammount = avatar:getCurrentInstructions()
         -- print("starting_ammount:", starting_ammount)
@@ -22,7 +21,7 @@ if host:isHost() then
 
         -- convert packets into one big long string, and wrap it in lua long quotes and a return statement so that we can load it later.
 
-        local packets_raw_file_name = "TL_song_exports/"..song.name..".raw_packets.lua"
+        local packets_raw_file_name = "TL_song_exports/" .. song.name .. ".raw_packets.lua"
         local packets_raw_base_path = packets_raw_file_name:gsub("/[^/]*$", "/")
         file:mkdirs(packets_raw_base_path)
 
@@ -32,13 +31,14 @@ if host:isHost() then
         local raw_file_string_table = {}
 
         -- Add some human readable info as comments.
-        table.insert(raw_file_string_table, "-- "..song.name.."\n")
-        table.insert(raw_file_string_table, "-- "..tostring(#song.processed_data.instructions).. " instructions".."\n")
-        table.insert(raw_file_string_table, "-- "..tostring(#packets).. " packets".."\n")
+        table.insert(raw_file_string_table, "-- " .. song.name .. "\n")
+        table.insert(raw_file_string_table,
+            "-- " .. tostring(#song.processed_data.instructions) .. " instructions" .. "\n")
+        table.insert(raw_file_string_table, "-- " .. tostring(#packets) .. " packets" .. "\n")
         table.insert(raw_file_string_table, "\n")
 
         -- Start of the packet strings table.
-        table.insert(raw_file_string_table, "local processed_song_data = {\n")  -- return ".. '"'.. song.name ..'"'
+        table.insert(raw_file_string_table, "local processed_song_data = {\n") -- return ".. '"'.. song.name ..'"'
 
         --- long quotes use `[` and `]`. These are magic characters in string.match, so we'll need to escape them.
         local function escape_match_magic_characters(str)
@@ -47,8 +47,8 @@ if host:isHost() then
             ---@see https://www.lua.org/manual/5.2/manual.html#pdf-package.searchers:~:text=Any%20punctuation%20character%20%28even%20the%20non%20magic%29%20can%20be%20preceded%20by%20a%20%27%25%27%20when%20used%20to%20represent%20itself%20in%20a%20pattern%2E
 
             return (str:gsub(
-                "([^%w])",  -- Gets all non-alpha-numeric characters
-                "%%%1")     -- appends a `%` to the capture.
+                "([^%w])", -- Gets all non-alpha-numeric characters
+                "%%%1")    -- appends a `%` to the capture.
             )
         end
 
@@ -67,32 +67,36 @@ if host:isHost() then
             --
             -- (https://www.lua.org/manual/5.2/manual.html#:~:text=long%20brackets)
 
-            local required_long_quote_level = -1    -- first loop will bump this to `0` for us
+            local required_long_quote_level = -1 -- first loop will bump this to `0` for us
             local opening_long_quote
             local closeing_long_quote
             local string_includes_these_long_quotes
             repeat
                 required_long_quote_level = required_long_quote_level + 1
-                opening_long_quote = "["..string.rep("=", required_long_quote_level).."["
-                closeing_long_quote = "]"..string.rep("=", required_long_quote_level).."]"
+                opening_long_quote = "[" .. string.rep("=", required_long_quote_level) .. "["
+                closeing_long_quote = "]" .. string.rep("=", required_long_quote_level) .. "]"
                 string_includes_these_long_quotes =
-                        string.match(unquoted_string, escape_match_magic_characters(closeing_long_quote))
-                    or  string.match(unquoted_string, escape_match_magic_characters(opening_long_quote))
+                    string.match(unquoted_string, escape_match_magic_characters(closeing_long_quote))
+                    or string.match(unquoted_string,
+                        escape_match_magic_characters(opening_long_quote))
             until not string_includes_these_long_quotes
 
-            return opening_long_quote..unquoted_string..closeing_long_quote
+            return opening_long_quote .. unquoted_string .. closeing_long_quote
         end
 
         -- Quote packets and add to table.
         for _, packet in ipairs(packets) do
-            table.insert(raw_file_string_table, safely_wrap_string_in_quotes(packet) ..",\n")
+            table.insert(raw_file_string_table, safely_wrap_string_in_quotes(packet) .. ",\n")
         end
 
         -- close processed_song_data table
         table.insert(raw_file_string_table, "}\n\n")
 
         -- return processed_song_data with some metadata
-        table.insert(raw_file_string_table, "return {data = processed_song_data, name = ".. safely_wrap_string_in_quotes(song.name) .."}")
+        table.insert(raw_file_string_table,
+            "return {data = processed_song_data, name = " ..
+            safely_wrap_string_in_quotes(song.name) .. "}"
+        )
 
         local final_string = table.concat(raw_file_string_table, "")
 
@@ -107,7 +111,6 @@ if host:isHost() then
 
         -- local baptized_info = require("./music_player/file_processors/local/local_songs/starbound-atlas.mid.raw_packets")
         -- printTable(baptized_info)
-
     end)
 end
 
@@ -128,10 +131,10 @@ end
 --       https://discord.com/channels/1129805506354085959/1340798228165300224/1340798228165300224
 --       /give @p minecraft:player_head[minecraft:profile={id:[I;1039887675,1961051688,-1756947787,-2031944347],name:"Drum"}]
 
-if host:isHost() then   -- TODO: it's host only b/c build_default_library() calls file API. should we instead have build_default_library() skip filesAPI if non-host? (possibly allows for local songs)
+if host:isHost() then -- TODO: it's host only b/c build_default_library() calls file API. should we instead have build_default_library() skip filesAPI if non-host? (possibly allows for local songs)
     local ui_api = require("scripts/music_player/ui")
     local default_library = require("scripts/music_player/libraries"):build_default_library()
     local enter_music_player_action_wheel_ui = ui_api.new_action_wheel_ui(default_library)
-    root_action_wheel_page:setAction(-1, enter_music_player_action_wheel_ui )
+    root_action_wheel_page:setAction(-1, enter_music_player_action_wheel_ui)
 end
 return root_action_wheel_page
