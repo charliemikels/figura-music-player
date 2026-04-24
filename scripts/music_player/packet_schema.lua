dae676d4de03eaf123d95c53755887ea9b5428ca
charlie <charlie@charliemikels.com>
2026-04-24 17:02:36 -0400

    With AI stuff, figured out a more robust way to represent schemas in lua tables.
    This is probably a lost cause. If the decoder (runs on the viewer) has
    to burn instructions navigating all of this table nonsense, then it's
    probably on worth it.

--- scripts/music_player/packet_schema.lua
+++ scripts/music_player/packet_schema.lua
@@ -8,16 +8,78 @@ local control_packet_codes = {
     remove = 2,     -- Delete a song from the transfered song list
 }
 
+local packet_ids = {
+    control = 0,   -- A very tiny packet to send a few simple control codes.
+    header = 1, -- Includeds initial like name, duration, track_types
+    data = 2,   -- Bulk of the packet stream
+    config = 3, -- A packet that might appear to update a song's configuration
+}
+
+-- ---@alias DataSchemaIntegerType {}
+
+-- ---@alias DataSchemaTypes ("integer"|"string"|"list"|"struct"|"switch")
+--
+-- ---@class DataSchema
+-- ---@field type DataSchemaTypes
+-- ---@field value table
+--
+-- ---@type DataSchema
+
+local instruction_schema = {
+    type = "struct",
+    fields = {
+        { name = "time_since_packet_start", schema = { type = "integer" } },
+        { name = "track_id", schema = { type = "integer" }},
+        { name = "body", schema = { type = "switch",
+            selector = {kind = "nil_field", name = "track_id"},
+            variants = {
+                is_nil = modifier_schema,
+                has_value = instruction_body_schema
+            }
+        }},
+    }
+}
+
+local data_schema = {
+    type = "struct",
+    fields = {
+        { name = "time_since_song_start", schema = { type = "integer" } },
+        { name = "instructions", schema = { type = "list", item = instruction_schema } }
+    }
+}
 
 
 local packet_schema = {
-    {data_type = "integer", source = nil, name = "Delta since start of song"},
-    {data_type = "table", source = {
-        {--[[ start_time ]]},
-        {--[[ instruction or modifier part. Is this a table part? ]]},
-        {--[[ idk ]]}
-    }, name = "instructions and modifiers"}
+    type = "struct",
+    fields = {
+        { name = "transfer_id", schema = { type = "integer" }},
+        { name = "packet_id", schema = { type = "integer" }},
+        { name = "body", schema = { type = "switch",
+            selector = { kind = "field", name = "packet_id" },
+            variants = {
+                [packet_ids.control] = control_schema,
+                [packet_ids.header]  = header_schema,
+                [packet_ids.data]    = data_schema,
+                [packet_ids.config]  = config_schema
+            }
+        }},
 
+        { name = "delta_time", schema = { type = "vlq" }},
+        { name = "packet", schema = { type = "vlq" }},
+
+
+        -- {key = "delta_time", type = "integer", value = ""},
+        -- {},
+
+
+
+        -- {data_type = "integer", source = nil, name = "Delta since start of song"},
+        -- {data_type = "table", source = {
+        --     {--[[ start_time ]]},
+        --     {--[[ instruction or modifier part. Is this a table part? ]]},
+        --     {--[[ idk ]]}
+        -- }, name = "instructions and modifiers"}
+    }
 }
 
 
