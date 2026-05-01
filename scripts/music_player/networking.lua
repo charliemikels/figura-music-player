8f61a2f2ec6118e13b012550d7bf5346b901b840
charlie <charlie@charliemikels.com>
2026-04-30 23:46:51 -0400

    exec decision to prevent viewers from makeing networked_song_players. (avoids double playback issues if host also called the function)

--- scripts/music_player/networking.lua
+++ scripts/music_player/networking.lua
@@ -368,8 +368,9 @@ end
 ---@return SongPlayerController
 local function new_network_song_player(processed_song, player_config)
     local song_player_api = require("./player")      ---@type SongPlayerAPI
-    if not host:isHost() then -- The caller is a viewer. They will not be able to do any syncronization with other clients, so just give them a normal player.
-        return song_player_api.new_player(processed_song, player_config)
+    if not host:isHost() then -- The caller is a viewer.
+        -- To avoid double-playback issues (where host makes a player, and also the viewer happens to make a player), this function will throw our own error
+        error("new_network_song_player was called, but caller is not Host. Use the normal song_player instead to let the viewer play songs.")
     end
 
     -- local our_song_player_controller = song_player_api.new_player(processed_song, player_config)
