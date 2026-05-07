2d39b91d173bbcd7b5906f96deb82fd29f6594d0
charlie <charlie@charliemikels.com>
2026-05-07 14:12:18 -0400

    shoot that list is short now.
    And both of these items are in extra-modular parts of the code. We can
    probably add them in without messing with the APIs

--- scripts/Action Wheel.lua
+++ scripts/Action Wheel.lua
@@ -49,17 +49,7 @@ end
 
 
 -- More or less: the current checklist
--- - [x] Ping Networking
--- - [x] UI
---   - [x] Store song configs with config API
--- - [x] Use commands to save a processed song so that it can be uploaded with the avatar
 -- - [ ] Port the ABC player to a new processor
--- - [x] Minecraft Note Block instruments
--- - [x] Figura Piano instrument
--- ~~- [ ] Load instruments from other avatars~~: Non goal: Finding dynamicly discoverable instruments, and makeing sure the clients see the same thing is going to be really rough. Let's just use wrappers like the Chloespaced out instrument.
--- - [ ] test if I can force the viewer to load an offline avatar by making them render a player head
---   - See also: Chloe Piano 2.0 → https://github.com/ChloeSpacedOut/figura-midi-player/blob/3c2888209ac75b1c0ec57c7ea4ca0b49aee291bb/ChloesMidiPlayerClientExample/midiPlayerClient.lua#L85-L90
--- - [x] Register callback functions through song controller for song end / meta event received / etc.
 -- - [ ] Figura Drum Kit instrument
 --       https://discord.com/channels/1129805506354085959/1340798228165300224/1340798228165300224
 --       /give @p minecraft:player_head[minecraft:profile={id:[I;1039887675,1961051688,-1756947787,-2031944347],name:"Drum"}]
