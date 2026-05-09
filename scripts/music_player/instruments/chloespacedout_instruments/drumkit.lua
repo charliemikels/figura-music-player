e0f045753d0ffd6a8e55927a85f4a57e8804a1a9
charlie <charlie@charliemikels.com>
2026-05-09 18:37:56 -0400

    significantly lowered drumkit volume.
    The kit is basicaly clipping, so all the detail in diffrent volume
    ranges are basicaly lost. See SSB4 Menu's opening hihat roll.

--- scripts/music_player/instruments/chloespacedout_instruments/drumkit.lua
+++ scripts/music_player/instruments/chloespacedout_instruments/drumkit.lua
@@ -324,7 +324,7 @@ local piano_builder = {
                         true,
                         nil,
                         instruction.start_velocity
-                            * 0.5                           -- Drum is a little loud by default reletive to the other instruments.
+                            * 0.02                         -- Drum is a little loud by default reletive to the other instruments.
                             * (avatar:getVolume() / 100)    -- Respect if viewer has muted the host.
                     )   -- playNote is kinda a legacy function for Piano 2.0, but it's the same signature for old and new drums.
 
