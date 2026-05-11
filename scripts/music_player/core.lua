8b18ce2a365afbb31c5bf264d584442602389b38
charlie <charlie@charliemikels.com>
2026-05-11 15:13:59 -0400

    core has been updated to return a UI action instead of a library

--- scripts/music_player/core.lua
+++ scripts/music_player/core.lua
@@ -67,16 +67,41 @@ end)
 ---@field recommended_instrument_name string? The name of an instrument for display in a UI
 
 ---@class CoreApi
----@field build_default_experiance fun():MusicPlayerCore    TODO: revisit this type declaration
 local core_api = {
-    build_default_experiance = function()
-        local library = require("./libraries"):build_default_library()
 
-        ---@class MusicPlayerCore
-        ---@field library Library
-        return {
-            library = library,
-        }
+    --- Returns an action that you can put into your Action Wheel to access the music player
+    ---
+    ---
+    --- Example:
+    ---
+    --- ```lua
+    --- local root_action_wheel_page = action_wheel:newPage()
+    --- action_wheel:setPage(root_action_wheel_page)
+    ---
+    --- local core = require("scripts.music_player.core")
+    --- local enter_songbook_action = core.build_default_ui_action()
+    ---
+    --- root_action_wheel_page:setAction(-1, enter_songbook_action)
+    --- ```
+    ---
+    --- or if you're crazy, you can do this in one line.
+    ---
+    --- ```lua
+    --- action_wheel:setPage(action_wheel:newPage():setAction(-1, require("scripts.music_player.core").build_default_ui_action()))
+    --- ```
+    ---
+    ---@return Action enter_songbook_action
+    build_default_ui_action = function()
+
+        local _ = require("./networking")   -- We want to make sure that the viewer definitly has the networking library ready to go.
+
+        local library_api = require("./libraries") ---@type LibrariesApi
+        local library = library_api:build_default_library()
+
+        local ui_api = require("./ui")  ---@type MusicPlayerUiAPI
+        local action = ui_api.new_action_wheel_ui(library)
+
+        return action
     end
 }
 
