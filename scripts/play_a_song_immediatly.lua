02cfe037c252cc431ecabf0df7163ebc93768a42
charlie <charlie@charliemikels.com>
2026-07-27 12:32:13 -0400

    fixed errores inherited from old logic

--- scripts/play_a_song_immediatly.lua
+++ scripts/play_a_song_immediatly.lua
@@ -11,13 +11,13 @@ events.ENTITY_INIT:register(function ()
         local song_processor_future = song:start_or_get_data_processor()
 
         song_processor_future:register_on_done_callback(function(done_future)
-            -- local done_song = done_future:get_value_or_throw_error()
+            local done_song = done_future:get_value_or_throw_error()
             local config_api = require("scripts/music_player/config_cache")     ---@type ConfigCacheAPI
             local music_player_api = require("scripts/music_player/song_player")     ---@type SongPlayerAPI
-            local song_config = config_api.load_song_config(song.id)
+            local song_config = config_api.load_song_config(done_song.id)
             song_config.source_entity = player
 
-            local song_player_controller = music_player_api.new_player(song.processed_song, song_config)
+            local song_player_controller = music_player_api.new_player(done_song, song_config)
 
             song_player_controller.play()
         end)
