5eab776d4f32df76a551a769097dc26d2405fd34
charlie <charlie@charliemikels.com>
2026-08-05 00:20:27 -0400

    upgraded play_song_immediatly to use the networked song player to test packet encodeing/decodeing.
    There is an error. I knew it would be too good to be true if it worked
    out of the box.

--- scripts/play_a_song_immediatly.lua
+++ scripts/play_a_song_immediatly.lua
@@ -15,11 +15,12 @@ events.ENTITY_INIT:register(function ()
         song_processor_future:register_on_done_callback(function(done_future)
             local done_song = done_future:get_value_or_throw_error()
             local config_api = require("scripts/music_player/config_cache")     ---@type ConfigCacheAPI
-            local music_player_api = require("scripts/music_player/song_player")     ---@type SongPlayerAPI
+            -- local music_player_api = require("scripts/music_player/song_player")     ---@type SongPlayerAPI
+            local networking_api = require("scripts/music_player/networking")     ---@type SongNetworkingApi
             local song_config = config_api.load_song_config(song.id)
             song_config.source_entity = player
 
-            local song_player_controller = music_player_api.new_player(done_song, song_config)
+            local song_player_controller = networking_api.new_network_player(done_song, song_config)
 
             song_player_controller.play()
         end)
