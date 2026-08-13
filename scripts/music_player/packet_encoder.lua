a68e46bd593ae05e17683baa74f66e65068cffbc
charlie <charlie@charliemikels.com>
2026-08-13 00:04:09 -0400

    flipped arround in case I did the logic wrong???

--- scripts/music_player/packet_encoder.lua
+++ scripts/music_player/packet_encoder.lua
@@ -604,7 +604,7 @@ local function build_data_packets_and_buffer_time(song)
                 (not last_added_track_instructions[instruction.track_index])    -- no TrackInstructions on this track. We can just add it now.
                 or (not last_added_track_instructions[instruction.track_index][instruction.type])   -- no TrackInstructions of this type on this track. We can just add it now.
                 or ( -- there must be a last_added instruction. if enough time has passed, we can add this instruction
-                    (instruction.start_time - last_added_track_instructions[instruction.track_index][instruction.type].start_time) > target_modifier_temporal_resolution
+                    (last_added_track_instructions[instruction.track_index][instruction.type].start_time + target_modifier_temporal_resolution) < instruction.start_time
                 )
             )
 
